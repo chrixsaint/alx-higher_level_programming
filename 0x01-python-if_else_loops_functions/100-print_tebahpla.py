060035dfca30245ad8821e186ded3e8f07c855c2
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-31 22:52:14 +0100

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -2):
-    print("{}{}".format(chr(i), chr((i - 1) - 32)), end='')
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
