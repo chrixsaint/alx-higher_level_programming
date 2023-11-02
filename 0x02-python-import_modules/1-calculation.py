adad85e8d9021aee6d8dd947b4474196966b3edd
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-11-02 13:33:49 +0100

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,4 +9,3 @@ if __name__ == "__main__":
     div = calc.div(a, b)
     print("{} + {} = {}\n{} - {} = {}".format(a, b, add, a, b, sub))
     print("{} * {} = {}\n{} / {} = {}".format(a, b, mul, a, b, div))
-
