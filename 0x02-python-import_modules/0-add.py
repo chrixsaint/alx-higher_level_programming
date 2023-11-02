be492a870702ce3a3a9380baeed4324257ef7a87
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-11-02 13:04:58 +0100

    task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    import add_0 as plus
+    a = 1
+    b = 2
+    c = plus.add(a, b)
+    print("{} + {} = {}".format(a, b, c))
