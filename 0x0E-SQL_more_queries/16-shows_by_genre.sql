b94db43f543fd2212455ab875865174ec3e91c54
christian ochenehi peter <christianochenehipeter@gmail.com>
2024-01-17 12:18:50 +0100

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- a script that lists all shows, and all genres linked to that show, from the database
+
+
+SELECT title, name
+FROM tv_shows
+LEFT  JOIN tv_shows_genres
+ON tv_shows.id = tv_shows_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_shows_genres.genre_id
+ORDER BY title ASC, name ASC;
