1d907c592bdbc9e22d1826bef0fd9e3b382afa97
christian ochenehi peter <christianochenehipeter@gmail.com>
2024-03-02 10:26:22 +0100

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+do this ./100-github_commits.py <repository name> <repository owner>
 """
-Python script that takes your GitHub credentials (username and personal access
-token) and uses the GitHub API to display your id.
-"""
-import requests
 import sys
+import requests
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        username = sys.argv[1]
-        token = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-        url = "https://api.github.com/user"
-        headers = {"Authorization": "Basic " + username + ":" + token}
-
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            print(data.get("id"))
-        else:
-            print(None)
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
